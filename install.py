17d68abf69dd8796010c9d4af38ff97accd68162
Debhole <67201361+Debhole@users.noreply.github.com>
2024-01-18 15:40:40 -0500

    improve lib installs

---
+++ install.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+import shutil
+from subprocess import call
+
+def git_clone(repo_url, repo_name, clean):
+    path = f"lib/{repo_name}"
+
+    if clean and os.path.isdir(path):
+        print(f"Cleaning {path}")
+        shutil.rmtree(path)
+
+    if not os.path.isdir(path):
+        print(f"{path} does not exist, installing it.")
+        call(["git", "clone", repo_url, path])
+    else:
+        print(f"{path} already exists, ignoring it.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="install dependencies")
+    parser.add_argument("--clean", dest="clean", action="store_true",
+                        help="clean the libs directory before installing")
+    args = parser.parse_args()
+
+    git_clone("git@github.com:catchorg/Catch2.git", "Catch2", args.clean)
+    git_clone("git@github.com:glfw/glfw.git", "glfw", args.clean)
